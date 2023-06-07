d5cbff512958c560949465c81988ee86d3217f52
Gift Segun Ade <giftsegunade@gmail.com>
2023-06-07 11:06:49 +0100

    i don tire

---
+++ views/index.php
@@ -0,0 +1,7 @@
+<?php include __DIR__ . '/../config/validate.php' ?>
+
+
+<?php
+$name = $_SESSION['name'] ?? 'Guest';
+echo "<h1>Welcome $name</h1>"
+?>
--- views/signup.php
+++ views/signup.php
@@ -1,21 +1,23 @@
 <?php include __DIR__ . '/../config/database.php'?>
 <?php include __DIR__ . "/../inc/header.php" ?>
+<?php include __DIR__ . '/../config/validate.php';
+          
+?>
 
-
-<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])  ?>" class="mt-4 d-flex flex-column w-75  p-5" method="POST">
+<form action="<?php echo htmlspecialchars('/config/validate.php' ) ?>" class="mt-4 d-flex flex-column w-75  p-5" method="POST">
   <!-- Name input  -->
   <div class="mb-3 w-50">
-    <input type="text" maxlength="50" class="form-control px-5  w-100" name="name" placeholder="Input name">
+    <input type="text" required maxlength="50" class="form-control px-5  w-100" name="name" placeholder="Input name">
   </div>
 
   <!-- Email input  -->
   <div class="mb-3 w-50">
-    <input type="email" class="form-control px-5  w-100" name="email" placeholder="Input email">
+    <input type="email" required class="form-control px-5  w-100" name="email" placeholder="Input email">
   </div>
 
   <!-- Password input  -->
   <div class="mb-3 w-50">
-    <input type="password" class="form-control px-5  w-100" name="password" placeholder="Input password">
+    <input type="password" required class="form-control px-5  w-100" name="password" placeholder="Input password">
   </div>
 
   <div class="mb-3 form-group">
